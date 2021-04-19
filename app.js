d8fc233652b8c4db3513f87f4eb280d1165bb868
Roshni <rnp9592@gmail.com>
2021-04-18 23:31:42 -0400

    v2

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://sa-5.herokuapp.com/auth/google/secrets",
+    callbackURL: "https://saa3.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
